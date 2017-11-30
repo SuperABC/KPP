2115db4dccf5ec79d149ec0b0df48ec9ec827365
SuperABC <1363028533@qq.com>
2017-11-30 10:01:33 +0800

    Update.

--- kpp.cpp
+++ kpp.cpp
@@ -1,16 +1,37 @@
 #include "main.h"
+#include "Music/phone.h"
 
-int dropDown;
+int state = KS_LAUNCH;
+
+extern Phone phone;
 
 void sgSetup() {
 	initWindow(800, 600, "Keyboard Piano Player", BIT_MAP);
 	setMenu();
+	setPanel();
+	initKey();
+	initMouse(SG_COORDINATE);
+	initMidi();
 }
 void sgLoop() {
-	disableItem(dropDown);
+	switch (state) {
+	case KS_LAUNCH:
+		refreshLayout();
+		state = KS_FREE;
+		break;
+	case KS_FREE:
+		freePlay();
+		break;
+	}
 }
 
 void nullFunc() {}
+void setVolume(widgetObj *w, int x, int y) {
+	mouseMoveDrag(w, x, y);
+	if (w->status&WIDGET_PRESSED) {
+		phone.setVolume(w->value);
+	}
+}
 void setMenu() {
 	initMenu();
 	int idFile = addMenuList("文件", 0);
@@ -30,7 +51,7 @@ void setMenu() {
 	addMenuItem("粘贴\tCtrl + V", idEdit, nullFunc);
 	addMenuItem("清空", idEdit, nullFunc);
 	addMenuItem("琴键", idView, nullFunc);
-	dropDown = addMenuItem("瀑布", idView, nullFunc);
+	addMenuItem("瀑布", idView, nullFunc);
 	addMenuItem("乐谱", idView, nullFunc);
 	addMenuItem("调音板", idView, nullFunc);
 	addMenuItem("文件", idView, nullFunc);
@@ -40,4 +61,60 @@ void setMenu() {
 	addMenuItem("键盘", idSetting, nullFunc);
 	addMenuItem("关于", idHelp, nullFunc);
 	addMenuItem("说明书", idHelp, nullFunc);
+}
+void setPanel() {
+	widgetObj *volume, *instrument, *sympho, *style, *lasting;
+
+	lasting = newWidget(SG_CHECK, SGstring("lasting"));
+	lasting->pos.x = 460;
+	lasting->pos.y = 336;
+	lasting->size.x = 48;
+	lasting->size.y = 20;
+	strcpy((char *)lasting->content, "延音");
+	registerWidget(lasting);
+	free(lasting);
+
+	instrument = newWidget(SG_LIST, SGstring("instrument"));
+	instrument->pos.x = 520;
+	instrument->pos.y = 334;
+	instrument->size.x = 60;
+	instrument->size.y = 20;
+	instrument->hide = 6;
+	memcpy((char *)instrument->content, "钢琴\0和声\0风琴\0吉他\0贝斯\0弦乐", 30);
+	registerWidget(instrument);
+	free(instrument);
+
+	sympho = newWidget(SG_LIST, SGstring("sympho"));
+	sympho->pos.x = 588;
+	sympho->pos.y = 334;
+	sympho->size.x = 40;
+	sympho->size.y = 20;
+	sympho->hide = 12;
+	memcpy((char *)sympho->content, "C\0#C\0D\0#D\0E\0F\0#F\0G\0#G\0A\0#A\0B", 29);
+	registerWidget(sympho);
+	free(sympho);
+
+	style = newWidget(SG_LIST, SGstring("style"));
+	style->pos.x = 632;
+	style->pos.y = 334;
+	style->size.x = 60;
+	style->size.y = 20;
+	style->hide = 12;
+	memcpy((char *)style->content, "M\0m\0\x37\0\x37M\0m7\0m7M\0\x36\0m6\0sus2\0sus4\0\x37sus2\0\x37sus4", 48);
+	registerWidget(style);
+	free(style);
+
+	volume = newWidget(SG_DRAG, SGstring("volume"));
+	volume->pos.x = 700;
+	volume->pos.y = 334;
+	volume->size.x = 80;
+	volume->size.y = 20;
+	volume->mouseIn = (mouseMoveCall)setVolume;
+	registerWidget(volume);
+	free(volume);
+}
+
+void refreshLayout() {
+	loadBmp(0, 0, "Source//kpp//琴键.bmp");
+	loadBmp(0, 360, "Source//kpp//键盘.bmp");
 }
