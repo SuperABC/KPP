da160a8440123f14e57955f3f914f154e751f1b7
SuperABC <1363028533@qq.com>
2017-11-30 10:01:46 +0800

    Update.

--- main.h
+++ main.h
@@ -2,8 +2,19 @@
 #define KPP_MAIN
 #include <winsgl.h>
 
+enum kppState {
+	KS_LAUNCH,
+	KS_FREE
+};
+
 void nullFunc();
+void setVolume(widgetObj *w, int x, int y);
 void setMenu();
+void setPanel();
+
+void refreshLayout();
+
+void freePlay();
 
 #endif
 
